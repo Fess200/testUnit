f0bf6c08641308d384c1ba71d5cdfc418bde389d
Katyushin Andrey <fess200@yandex.ru>
2014-05-27 14:00:29 +0400

    Initial Commit

---
+++ testXCtest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testXCtest
+//
+//  Created by Andrew on 27.05.14.
+//  Copyright (c) 2014 Irevolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testXCtest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testXCtest
+//
+//  Created by Andrew on 27.05.14.
+//  Copyright (c) 2014 Irevolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
